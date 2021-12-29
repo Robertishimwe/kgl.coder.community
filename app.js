96f9a07ff7593cacf3bf4cc80a99a0ec34c86461
Robertisimwe <robertishimwe0@gmail.com>
2021-12-29 17:24:00 +0000

    landing page

---
+++ app.js
@@ -0,0 +1,15 @@
+function toJadenCase() {
+    //...
+    let str = "how can mirrors be real if our eyes aren't real";
+    arr = str.split(" ");
+    for(let i=0;i<arr.length;i++){
+        let arr2 = arr[i].replace([0].toUpperCase());
+        console.log(arr2)
+        
+
+    }
+    
+      
+  };
+  toJadenCase();
+  
