57132b35c5e304d86081640557f72c431d352614
robert <robertishimwe0@gmail.com>
2022-09-12 21:39:45 +0200

    test
    test

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function toJadenCase() {
     //...
     let str = "how can mirrors be real if our eyes aren't real";
+    // /sjkdhkjh
     arr = str.split(" ");
     for(let i=0;i<arr.length;i++){
+        // skdjhkjsdhkjsd
         let arr2 = arr[i].replace([0].toUpperCase());
+        //djhsdkjhkjsds
         console.log(arr2)
         
 
